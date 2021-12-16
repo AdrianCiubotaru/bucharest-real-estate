2c2fd77bffaef4a1dcf3ded92c617f97058add41
Adrian Ciubotaru <adrian.ciubotaru47@gmail.com>
2021-12-16 22:24:25 +0200

    data cleaning notebook 1.1

---
+++ tests.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+
+def _print_success_message():
+    print('Tests passed!')
+    
+def test_rent_removed(input_df):
+    assert input_df['price'].apply(lambda x: '/ lună' in x).sum() == 0
+    _print_success_message()
+
+    
+def test_price_values(input_df):
+    assert input_df['eur_price'].isnull().sum() == 0
+    _print_success_message()
