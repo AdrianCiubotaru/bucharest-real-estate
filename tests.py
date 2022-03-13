dd8d4802c567a96616a692eefc9ab84ba899eb67
EC2 Default User <ec2-user@ip-172-16-104-42.ec2.internal>
2022-03-13 09:01:47 +0000

    model deployed

--- tests.py
+++ tests.py
@@ -7,8 +7,13 @@ def _print_success_message():
 def test_rent_removed(input_df):
     assert input_df['price'].apply(lambda x: '/ lună' in x).sum() == 0
     _print_success_message()
-
     
-def test_price_values(input_df):
-    assert input_df['eur_price'].isnull().sum() == 0
+    
+def test_column_has_only_accepted_values(input_df, input_col, accepted_values):
+    assert all([val in accepted_values for val in input_df[input_col].unique()])
+    _print_success_message()
+    
+    
+def test_column_has_no_null_values(input_df, input_col):
+    assert input_df[input_col].isnull().sum() == 0
     _print_success_message()
